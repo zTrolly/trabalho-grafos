af5849b34b8dc4d90d7d396a13aec7a817d39a4d
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2022-10-19 17:37:15 -0300

    teste inicial

--- main.py
+++ main.py
@@ -65,6 +65,7 @@ def listaAdjacencia(arestas, vertices):
         listaAdjacencia[arestas[i][1]].append(arestas[i][0])
     return listaAdjacencia
 
+
 def tarjan(graph):
     if len(graph) == 0:
         return []
@@ -76,24 +77,62 @@ def tarjan(graph):
     low = [float('inf')] * n
     disc = [float('inf')] * n
     parent = [-1] * n
-    def dfs(u, disc, low, parent, visited, bridges):
+    
+    queue = []  # Initialize a queue
+    def bfs(u, visited):
         visited[u] = True
         disc[u] = low[u] = next(time)
-        for v in graph[u]:
-            if not visited[v]:
-                parent[v] = u
-                dfs(v, disc, low, parent, visited, bridges)
-                low[u] = min(low[u], low[v])
-                if low[v] > disc[u]:
-                    bridges.append([u, v])
-                    bridges.append([v, u])
-            elif v != parent[u]:
-                low[u] = min(low[u], disc[v])
+        queue.append(u)
+        while queue:          # Creating loop to visit each node
+            u = queue.pop(0)
+            for v in graph[u]:
+                if not visited[v]:
+                    visited[v] = True
+                    queue.append(v)
+                    
+                    parent[v] = u
+                    low[u] = min(low[u], low[v])
+                    if low[v] > disc[u]:
+                        bridges.append([u, v])
+                        bridges.append([v, u])
+                elif v != parent[u]:
+                    low[u] = min(low[u], disc[v])
     time = iter(range(n))
+    
     for u in range(n):
         if not visited[u]:
-            dfs(u, disc, low, parent, visited, bridges)
+            bfs(u, visited)
     return bridges
+    
+# def tarjan(graph):
+#     if len(graph) == 0:
+#         return []
+#     if len(graph) == 1:
+#         return []
+#     bridges = []
+#     n = len(graph)
+#     visited = [False] * n
+#     low = [float('inf')] * n
+#     disc = [float('inf')] * n
+#     parent = [-1] * n
+#     def dfs(u, disc, low, parent, visited, bridges):
+#         visited[u] = True
+#         disc[u] = low[u] = next(time)
+#         for v in graph[u]:
+#             if not visited[v]:
+#                 parent[v] = u
+#                 dfs(v, disc, low, parent, visited, bridges)
+#                 low[u] = min(low[u], low[v])
+#                 if low[v] > disc[u]:
+#                     bridges.append([u, v])
+#                     bridges.append([v, u])
+#             elif v != parent[u]:
+#                 low[u] = min(low[u], disc[v])
+#     time = iter(range(n))
+#     for u in range(n):
+#         if not visited[u]:
+#             dfs(u, disc, low, parent, visited, bridges)
+#     return bridges
 
 def naive_bridge(grafo):
     pontes = list()
